dbb77f4b2a653dd3c0d3719a4f4eb99eb0e7b38a
Manteiga <rafael.manteiga@hotmail.com>
2023-07-25 03:32:54 -0300

    Refatorado

--- jogodavelha.py
+++ jogodavelha.py
@@ -1,59 +1,49 @@
-#Document properties
-#__author__ = "Alicia Neri de Araujo Manteiga"
-#__copyright__ = "Copyright_2021"
-#__credits__ = __author__
-#__license__ = "GPL"
-#__version__ = "1.0.0"
-#__maintainer__ = "Alicia_Manteiga"
-
-#__email__ = "alicia.sk3@poli.ufrj.br
-#__status__ = "Production"
-
 import metcomunicausuario
 import metfuncjogo
 
-def main():
+def jogar_novamente():
+    retorno = metcomunicausuario.joganovamente()
+    return retorno == 1
 
-        j1,j2 = metcomunicausuario.digitanome()
+def exibir_tabuleiro(jogo):
+    for linha in jogo:
+        print(" ".join(linha))
+    print()
 
-        jogo = [["_","_","_"],["_","_","_"],["_","_","_"]]
-
-        for i in range(3):
-            print(" ".join(jogo[i]))
-        print("")
-
-        nj = 0
-        print("\nÉ a vez do", j1)
+def jogar(jogador, jogo):
+    if jogador == "X":
         metcomunicausuario.xjoga(jogo)
-        for i in range(3):
-            print(" ".join(jogo[i]))
-        #if testavitoria(jogo,j1):
-        #    print("\nParabéns, "+ j1 +"! Você venceu!")
-        while(nj!=4):
-            nj=nj+1
-            print("\nÉ a vez do", j2)
-            metcomunicausuario.ojoga(jogo)
-            for i in range(3):
-                print(" ".join(jogo[i]))
-            if metfuncjogo.testavitoria(jogo,j2):
-                print("\nParabéns, "+ j2 +"! Você venceu!")
-                break 
-            print("\nÉ a vez do", j1)
-            metcomunicausuario.xjoga(jogo)
-            for i in range(3):
-                print(" ".join(jogo[i]))
-            if metfuncjogo.testavitoria(jogo,j1):
-                print("\nParabéns, "+ j1 +"! Você venceu!")
-                break
+    elif jogador == "O":
+        metcomunicausuario.ojoga(jogo)
 
-        if(metfuncjogo.testavitoria(jogo,j2)==False and metfuncjogo.testavitoria(jogo,j1)==False):
-            print("\nDeu velha!")
+def verificar_vitoria(jogador, jogo):
+    return metfuncjogo.testavitoria(jogo, jogador)
 
-        retorno = metcomunicausuario.joganovamente()
-        if retorno == 1:
-                main()
-        else:
-                print("\nPrograma encerrado")
+def main():
+    j1, j2 = metcomunicausuario.digitanome()
 
-main()
-input()
+    jogo = [["_"] * 3 for _ in range(3)]
+
+    jogadores = [(j1, "X"), (j2, "O")]
+    
+    exibir_tabuleiro(jogo)
+    
+    for rodada in range(9):
+        jogador, simbolo = jogadores[rodada % 2]
+        print(f"\nÉ a vez do {jogador}")
+        jogar(simbolo, jogo)
+        exibir_tabuleiro(jogo)
+        
+        if verificar_vitoria(simbolo, jogo):
+            print(f"\nParabéns, {jogador}! Você venceu!")
+            break
+    else:
+        print("\nDeu velha!")
+
+    if jogar_novamente():
+        main()
+    else:
+        print("\nPrograma encerrado")
+
+if __name__ == "__main__":
+    main()
